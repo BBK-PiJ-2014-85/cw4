a57d90ecdc53fbf549c5b5b00a67698a69bf9c66
Paul Day <pday02@mail.bkk.ac.uk>
2015-02-15 13:59:16 +0000

    Class added with empty methods to allow compiling of tests

---
+++ impls/PastMeetingImpl.java
@@ -0,0 +1,35 @@
+package impls;
+
+import java.util.Calendar;
+import java.util.Set;
+
+import interfaces.Contact;
+import interfaces.PastMeeting;
+
+public class PastMeetingImpl implements PastMeeting {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
