e56392f2866d18f628b2722e22471ad2a0ad2937
Paul Day <pday02@mail.bkk.ac.uk>
2015-03-12 16:01:39 +0000

    Made the class extend MeetingImpl

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -3,24 +3,9 @@ import java.util.Calendar;
 import java.util.Set;
 
 
-public class FutureMeetingImpl implements FutureMeeting {
+public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting  {
 
-	@Override
-	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
+	public FutureMeetingImpl(int id, Calendar date, Set<Contact> attendees) {
+		super(id, date, attendees);
 	}
-
-	@Override
-	public Calendar getDate() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<Contact> getContacts() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
