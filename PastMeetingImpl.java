98656a0b31deb2e6c90383355d67a24506a018fe
Paul Day <pday02@mail.bkk.ac.uk>
2015-03-17 16:52:30 +0000

    Minor fix to contstructor using meeting to deal with null notes properly, removed redundant meetings from class and added javadoc and notes

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -1,30 +1,48 @@
 
 
 import java.util.Calendar;
-import java.util.List;
 import java.util.Set;
 
+/**
+ * An implementation of PastMeeting by extending MeetingImpl.
+ * 
+ * Holds and returns the id, date, list of attendees and notes of the meeting. 
+ * 
+ * This class does not ensure that ID's are unique, and therefore has a dependency on another class to ensure that IDs provided to this are unique.
+ * 
+ * ContactManagerImpl ensures IDs provided to this are unique.
+ * 
+ * @author Paul Day
+ */
 
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 
 	String notes = "";
 	
+	/**
+	 * A constructor to turn a Meeting into and PastMeeting. Note this does not delete the previous meeting but does duplicate its
+	 * ID. The user should therefore ensure deletion of the previous meeting.
+	 * 
+	 * @param currentMeeting the meeting to be turned into a PastMeeting
+	 * @param notes the notes for this meeting
+	 */
+	
 	public PastMeetingImpl(Meeting currentMeeting, String notes) {
 		super(currentMeeting.getId(),currentMeeting.getDate(),currentMeeting.getContacts());
-		this.notes = notes;
+		if (notes != null) this.notes = notes;
 	}
-
-	public PastMeetingImpl(int id, Calendar date, Set<Contact> attendees) {
-		super(id, date, attendees);
-	}
+	
+	/**
+	 * A constructor to produce a new PastMeeting. ID not guaranteed to be unique.
+	 * 
+	 * @param id the id of the meeting
+	 * @param date the date the meeting took place
+	 * @param attendees the attendees of the meeting
+	 * @param notes the note of the meeting
+	 */
 	
 	public PastMeetingImpl(int id, Calendar date, Set<Contact> attendees, String notes) {
 		super(id, date, attendees);
-		if (notes != null) this.notes = notes;
-	}
-
-	public PastMeetingImpl(PastMeeting meeting, String notes) {
-		super(meeting.getId(), meeting.getDate(), meeting.getContacts());
 		if (notes != null) this.notes = notes;
 	}
 
